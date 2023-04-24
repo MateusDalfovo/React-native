10fd9ca845ef91eee2c5adf89663a75d459c62db
MateusDalfovo <125157446+MateusDalfovo@users.noreply.github.com>
2023-04-24 10:05:45 -0300

    Add files via upload

---
+++ tela-login-surfview/tela-aplicativo-surfview/components/AssetExample.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+
+export default function AssetExample() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+       
+      </Text>
+      <Image style={styles.logo} source={require('../assets/surfview-logo.png')} />
+      <Image style={styles.login} source={require('../assets/logo-usuario.png')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+    backgroundColor: '#000000',
+
+  },
+  paragraph: {
+    margin: 10,
+    marginTop: -380,
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+
+  },
+  logo: {
+    flex: 1,
+    justifyContent: 'row',
+    alignSelf: 'center',
+    marginTop: -100,
+    height: 140,
+    width: 140,
+  },
+
+  login: {
+    marginTop: 10,
+    height: 40,
+    width: 40,
+  },
+});
